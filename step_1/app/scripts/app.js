bf5493ba6569b7915451808db3860e225bafef3b
Sandeep Wadhwa <sandeep.sajiru@gmail.com>
2015-08-29 11:43:41 +0530

    Scaffolding of Project Done

---
+++ step_1/app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name step1App
+ * @description
+ * # step1App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('step1App', []);
---
+++ step_1/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name step1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the step1App
+ */
+angular.module('step1App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
